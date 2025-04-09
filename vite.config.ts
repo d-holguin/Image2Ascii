292bd7c8a14cd66b14bae8d57da745520725045b
daniel holguin <d.holguin@protonmail.com>
2025-04-09 12:31:20 -0700

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://d-holguin.github.io/Image2Ascii/'
+  base: '/Image2Ascii/'
 })
